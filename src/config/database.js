eeee47141081efa1ba6fb2d73c963574eefda535
LucasE2996 <moeda.60@gmail.com>
2019-08-25 22:42:50 -0300

    Criando loader de models

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    dialect: 'posgres',
+    dialect: 'postgres',
     host: 'localhost',
     username: 'postgres',
     password: 'docker',
--- src/routers.js
+++ src/routers.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World!' }));
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Lucas Rosa',
+        email: 'lucas.rosa@wintaylor.com',
+        password_hash: '1234556677',
+    });
+
+    return res.json(user);
+});
 
 export default routes;
