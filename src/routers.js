029d859c5040e42624faf88ec688819f74f0a3b1
LucasE2996 <moeda.60@gmail.com>
2019-08-26 22:58:33 -0300

    Gerande hash da senha

--- src/routers.js
+++ src/routers.js
@@ -1,16 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-    const user = await User.create({
-        name: 'Lucas Rosa',
-        email: 'lucas.rosa@wintaylor.com',
-        password_hash: '1234556677',
-    });
-
-    return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
