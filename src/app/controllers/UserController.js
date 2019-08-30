d953fa38cf3ceb74d86f3799026fbde1496ab1bb
LucasE2996 <moeda.60@gmail.com>
2019-08-29 22:09:58 -0300

    update de usuario

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -20,8 +20,39 @@ class UserController {
         });
     }
 
-    update(req, res) {
-        return res.json({ ok: 'true' });
+    async update(req, res) {
+        const { email, oldPassword } = req.body;
+
+        const user = await User.findByPk(req.userId);
+
+        if (!user) {
+            return res.status(401).json({ error: 'User not found' });
+        }
+
+        if (email !== user.email) {
+            const userExists = await User.findOne({
+                where: { email },
+            });
+
+            if (userExists) {
+                return res.status(400).json({ error: 'User already exists' });
+            }
+        }
+
+        if (oldPassword && !(await user.checkPassword(oldPassword))) {
+            return res
+                .status(401)
+                .json({ error: 'Old password does not match' });
+        }
+
+        const { id, name, provider } = await user.update(req.body);
+
+        return res.json({
+            id,
+            name,
+            email,
+            provider,
+        });
     }
 }
 
