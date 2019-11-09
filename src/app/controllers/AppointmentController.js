72f418d510cb3951355acbf8e7a9590deff0d8ac
LucasE2996 <moeda.60@gmail.com>
2019-11-08 22:48:51 -0300

    list appointments

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,9 +1,38 @@
 import * as Yup from 'yup';
-
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
+import File from '../models/File';
 import Appointment from '../models/Appointment';
 
 class AppoitmentController {
+    async index(req, res) {
+        const { page = 1 } = req.query;
+
+        const appointments = await Appointment.findAll({
+            where: { user_id: req.userId, cancelled_at: null },
+            order: ['date'],
+            attributes: ['id', 'date'],
+            limit: 20,
+            offset: (page - 1) * 20,
+            include: [
+                {
+                    model: User,
+                    as: 'provider',
+                    attributes: ['id', 'name'],
+                    include: [
+                        {
+                            model: File,
+                            as: 'avatar',
+                            attributes: ['id', 'path', 'url'],
+                        },
+                    ],
+                },
+            ],
+        });
+
+        return res.json(appointments);
+    }
+
     async store(req, res) {
         const schema = Yup.object().shape({
             provider_id: Yup.number().required(),
@@ -21,7 +50,27 @@ class AppoitmentController {
         });
 
         if (!isProvider) {
-            return res.status(400).json({ error: 'You should be a provider to create appointments' });
+            return res.status(401).json({ error: 'You should be a provider to create appointments' });
+        }
+
+        const hourStart = startOfHour(parseISO(date));
+
+        if (isBefore(hourStart, new Date())) {
+            return res.status(400).json({ error: 'Past dates are not permited' });
+        }
+
+        const checkAvailability = await Appointment.findOne({
+            where: {
+                provider_id,
+                cancelled_at: null,
+                date,
+            },
+        });
+
+        console.log(checkAvailability);
+
+        if (checkAvailability) {
+            return res.status(400).json({ error: 'Already have one appointment with this date' });
         }
 
         const appointment = await Appointment.create({
--- src/routers.js
+++ src/routers.js
@@ -19,6 +19,7 @@ routes.use(authMiddleware); // routes bellow needs authentication
 
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
+routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.post('/files', upload.single('file'), FileController.store);
 
