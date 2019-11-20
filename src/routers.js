932c7d818738c88a62be68440ddf27dd3bba6939
LucasE2996 <moeda.60@gmail.com>
2019-11-20 19:41:45 -0300

    apply cancel appointment feature

--- src/routers.js
+++ src/routers.js
@@ -23,6 +23,7 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 routes.get('/schedules', ScheduleController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/notification', NotificationController.index);
