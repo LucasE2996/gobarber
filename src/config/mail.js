6012e2eee99abd4743399b1501c7eaf8bf4086d9
LucasE2996 <moeda.60@gmail.com>
2019-12-08 15:49:02 -0300

    mail

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: '2525',
+    secure: false,
+    auth: {
+        user: 'ca85874ca03479',
+        pass: '4156aa5bed1c27',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>',
+    },
+};
