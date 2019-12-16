9163fc8f2ef6d89810d4d6c0df3a0989c4026ac2
LucasE2996 <moeda.60@gmail.com>
2019-12-16 20:50:27 -0300

    send email when cancel appointment

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,6 +6,8 @@ import File from '../models/File';
 import Appointment from '../models/Appointment';
 import Notification from '../schemas/Notification';
 
+import Mail from '../../lib/Mail';
+
 class AppoitmentController {
     async index(req, res) {
         const { page = 1 } = req.query;
@@ -98,7 +100,16 @@ class AppoitmentController {
     }
 
     async delete(req, res) {
-        const appointment = await Appointment.findByPk(req.params.id);
+        const appointment = await Appointment.findByPk(req.params.id, {
+            // inlcudes provider info
+            include: [
+                {
+                    model: User,
+                    as: 'provider',
+                    attributes: ['name', 'email'],
+                },
+            ],
+        });
 
         if (appointment.user_id !== req.userId) {
             return res.status(401).json({ error: 'You do not have permission to delete this appointment' });
@@ -113,6 +124,12 @@ class AppoitmentController {
         appointment.cancelled_at = new Date();
 
         await appointment.save();
+
+        await Mail.sendMail({
+            to: `${appointment.provider.name} <${appointment.provider.email}>`,
+            subject: 'Agendamento cancelado',
+            text: 'Voce tem um novo cancelamento',
+        });
 
         return res.json(appointment);
     }
