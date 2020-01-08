a6b9477e226490c6b270bff5fe7b966a393161c2
LucasE2996 <moeda.60@gmail.com>
2020-01-07 21:56:14 -0300

    add error handler

--- src/app.js
+++ src/app.js
@@ -1,18 +1,26 @@
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
 import routes from './routers';
 
+import sentryConfig from './config/sentry';
 import './database';
 
 class App {
     constructor() {
         this.server = express();
 
+        Sentry.init(sentryConfig);
+
         this.middlewares();
         this.routes();
+        this.exceptionHandler();
     }
 
     middlewares() {
+        this.server.use(Sentry.Handlers.requestHandler());
         this.server.use(express.json());
         // expose files path to return static files like images
         this.server.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')));
@@ -20,6 +28,15 @@ class App {
 
     routes() {
         this.server.use(routes);
+        this.server.use(Sentry.Handlers.errorHandler());
+    }
+
+    exceptionHandler() {
+        this.server.use(async (err, req, res, next) => {
+            const errors = await new Youch(err, req).toJSON();
+
+            return res.status(500).json(errors);
+        });
     }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+    dsn: 'https://d0fd90303941470bbe481e8233ad515c@sentry.io/1872140',
+};
