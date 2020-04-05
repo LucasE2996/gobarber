da1b4938e583ab32b01fd903a9812c9f3ddbe7e3
Lucas Rosa <moeda.60@gmail.com>
2020-04-05 16:59:24 -0300

    add frontend project

---
+++ frontend/src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Container from '../../components/Container';
+import { Title } from './styles';
+
+export default function Main() {
+    return (
+        <Container>
+            <Title>Main component</Title>
+            <p>Subtitle with HTML tag</p>
+        </Container>
+    );
+}
